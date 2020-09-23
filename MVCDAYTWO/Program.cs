9927451aee8fdcb001e9a98ecb9eebfcb96e7420
bibhutighimire <ghimirebibhuti@gmail.com>
2020-09-22 18:14:21 -0600

    Add project files.

---
+++ MVCDAYTWO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVCDAYTWO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
