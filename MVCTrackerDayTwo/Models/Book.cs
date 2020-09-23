b1badf048c2d477883ed9ca25938bf176722b353
bibhutighimire <ghimirebibhuti@gmail.com>
2020-09-22 21:07:16 -0600

    changed to mvc and added context and model

---
+++ MVCTrackerDayTwo/Models/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MVCDAYTWO.Models
+{
+    [Table("Book")]
+    public partial class Book
+    {
+
+        [Key]
+        // Specifies AUTO_INCREMENT.
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id", TypeName = "int(10)")]
+        public int ID { get; set; }
+
+        [Required]
+        [Column("title", TypeName = "varchar(30)")]
+        public string Title { get; set; }
+
+        [Column("publicationdate", TypeName = "date")]
+        [Required]
+        public DateTime PublicationDate { get; set; }
+
+        [Column("checkedoutdate", TypeName = "date")]
+        [Required]
+        public DateTime CheckedOutDate { get; set; }
+
+        [Column("duedate", TypeName = "date")]
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Column("returnedate", TypeName = "date")]
+
+        public DateTime? ReturnedDate { get; set; }
+
+        [Column("author_id", TypeName = "int(10)")]
+        public int AuthorID { get; set; }
+
+        [ForeignKey(nameof(AuthorID))]
+        [InverseProperty(nameof(Models.Author.Books))]
+        public virtual Author Author { get; set; }
+    }
+}
+
---
+++ MVCTrackerDayTwo/Models/LibraryContext.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCDAYTWO.Models
+{
+    public partial class LibraryContext: DbContext
+    {                
+        public LibraryContext()
+        {
+
+        }
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // If we aren't already configured.
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseMySql("server=localhost;port=3306;user=root;database=mvc_library", x => x.ServerVersion("10.4.14-MariaDB"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                entity.HasData(
+                    new Author() { ID = -1, Name = "Author One"},
+                    new Author() { ID = -2, Name = "Author Two"},
+                    new Author() { ID = -3, Name = "Author Three"},
+                    new Author() { ID = -4, Name = "Author Four" },
+                    new Author() { ID = -5, Name = "Author Five"}
+                    );
+            });
+
+            // Declare advanced column configuration for our model.
+            modelBuilder.Entity<Book>(entity =>
+            {
+                // If you have foreign keys, declare them here as "HasIndex".
+                entity.HasIndex(e => e.AuthorID)
+                    .HasName("FK_Of_Author");
+
+                // PLEASE don't try to memorize this. Copy/paste it and change the column name.
+                entity.Property(e => e.Title)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_general_ci");
+
+                //entity.Property(e => e.PublicationDate)
+                //    .HasCharSet("utf8mb4")
+                //    .HasCollation("utf8mb4_general_ci");
+
+                //entity.Property(e => e.CheckedOutDate)
+                //    .HasCharSet("utf8mb4")
+                //    .HasCollation("utf8mb4_general_ci");
+
+                //entity.Property(e => e.DueDate)
+                //    .HasCharSet("utf8mb4")
+                //    .HasCollation("utf8mb4_general_ci");
+
+                //entity.Property(e => e.ReturnedDate)
+                //    .HasCharSet("utf8mb4")
+                //    .HasCollation("utf8mb4_general_ci");
+
+
+                // Enforce the Foreign Key
+                // Specify the relationship between the child and parent
+                entity.HasOne(child => child.Author)
+                    // Specify the relationship between the parent and child(ren)
+                    .WithMany(parent => parent.Books)
+                    // Specify the property acting as the foreign key
+                    .HasForeignKey(child => child.AuthorID)
+                    // Specify delete behaviour
+                    .OnDelete(DeleteBehavior.Cascade)
+                    // Name the foreign key
+                    .HasConstraintName("FK_Of_Author");
+
+                entity.HasData(
+                    new Book()
+                    {
+                        ID = -1,
+                        Title = "Title One",
+                        PublicationDate = new DateTime(2012, 12, 25),
+                        CheckedOutDate = new DateTime(2018, 12, 25),
+                        DueDate = new DateTime(2018, 12, 25),
+                        ReturnedDate = new DateTime(2018, 12, 25),
+                        AuthorID = -2
+                    },
+                    new Book()
+                    {
+                        ID = -2,
+                        Title = "Title Two",
+                        PublicationDate = new DateTime(2010, 12, 25),
+                        CheckedOutDate = new DateTime(2018, 12, 25),
+                        DueDate = new DateTime(2018, 12, 25),
+                        ReturnedDate = null,
+                        AuthorID = -2
+                    },
+                     new Book()
+                     {
+                         ID = -3,
+                         Title = "Title Three",
+                         PublicationDate = new DateTime(2008, 12, 25),
+                         CheckedOutDate = new DateTime(2018, 12, 25),
+                         DueDate = new DateTime(2018, 12, 25),
+                         ReturnedDate = new DateTime(2018, 12, 25),
+                         AuthorID = -2
+                     });
+
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
